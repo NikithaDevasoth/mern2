9055779b3749ea165dde0bc9d842013920e8431c
NikithaDevasoth <devasothnikitha09@gmail.com>
2024-10-27 20:25:13 +0530

    Day 6
    Rout-Project

---
+++ React/rout-project/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <>
+            <p>Iam Nikitha Devasoth</p>
+        </>
+    )
+}
+
+export default Home
--- React/sample-project/src/componets/Navbar.jsx
+++ React/sample-project/src/componets/Navbar.jsx
@@ -1,15 +1,24 @@
-import React ,{useState}from 'react'
+import React ,{useEffect, useState}from 'react'
 
 const Navbar = ({appdata,logo}) => {
-    const [data,setDataState]=useState(2)
-    const [stringdata,setStringState]=useState("b")
-    const [arraystate,setArrayState]=useState([89,"true","a"])
-    console.log(data)
-    console.log(stringdata)
-    console.log(arraystate)
+    // const [data,setDataState]=useState(2)
+    // const [stringdata,setStringState]=useState("b")
+    // const [arraystate,setArrayState]=useState([89,"true","a"])
+    // console.log(data)
+    // console.log(stringdata)
+    // console.log(arraystate)
+    const [count,setCount]=useState(6)
+    
+    useEffect(()=>{ alert('effect rendered')
+      // console.log('effect rendered')
+      console.log("Count is" + count)
+    },[count])
   return (
     <>
-    {appdata.appname} {logo}
+    {/* {appdata.appname} {logo} */}
+    <button className='w-[6rem] h-[6rem] bg-blue-500 text-white' onClick={()=>{setCount(count+1)}}>
+    Count{count}
+    </button>
     </>
   )
 }
