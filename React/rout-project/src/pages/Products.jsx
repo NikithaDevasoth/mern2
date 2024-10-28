b56105744eb6d8ea00704ebb8527fbbed486172b
NikithaDevasoth <devasothnikitha09@gmail.com>
2024-10-28 16:46:25 +0530

    Day-6
    products design added

--- React/rout-project/src/pages/Products.jsx
+++ React/rout-project/src/pages/Products.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
+    <>
+    <div className='w-screen h-full flex justify-center items-center'>
+    <div className="relative group rounded-lg w-64 bg-gray-50 overflow-hidden before:absolute before:w-12 before:h-12 before:content[''] before:right-0 before:bg-violet-500 before:rounded-full before:blur-lg before:[box-shadow:-60px_20px_10px_10px_#F9B0B9]">
+    
+  /* From Nikitha's Source */ 
+  <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
+    <div className="wheel" />
+    <div className="hamster">
+      <div className="hamster__body">
+        <div className="hamster__head">
+          <div className="hamster__ear" />
+          <div className="hamster__eye" />
+          <div className="hamster__nose" />
+        </div>
+        <div className="hamster__limb hamster__limb--fr" />
+        <div className="hamster__limb hamster__limb--fl" />
+        <div className="hamster__limb hamster__limb--br" />
+        <div className="hamster__limb hamster__limb--bl" />
+        <div className="hamster__tail" />
+      </div>
+    </div>
+    <div className="spoke" />
+    </div>
+  </div>
+ 
+  </div>
+        </>
+    )
 }
 
 export default Products
